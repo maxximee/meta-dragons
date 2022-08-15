b7c9b7d1a10aaa5cf0b82066b75e5ff3952cca58
gheysenm <maxime.gheysen@post.ch>
2022-08-15 15:38:16 +0700

    hello world empty site

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    'app-ads.txt'
   ],
   darkMode: 'class', // or 'media' or 'class'
   presets: [require('./utils/tailwind-preset')],
